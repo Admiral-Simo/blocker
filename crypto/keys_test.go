07cbfc1332297e0646ba8c61fced11f52f58ecb7
Admiral-Simo <admiralsimo16@gmail.com>
2024-07-19 19:40:00 +0100

    generate keys

--- crypto/keys_test.go
+++ crypto/keys_test.go
@@ -1,1 +1,16 @@
 package crypto
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGeneratePrivateKey(t *testing.T) {
+	privKey := GeneratePrivateKey()
+
+	assert.Equal(t, privKeyLen, len(privKey.Bytes()))
+	fmt.Println("privateKey :", privKey.Bytes())
+	fmt.Println("publicKey :", privKey.Public().key)
+}
