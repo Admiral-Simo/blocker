264057a15b3601b63d9ec883af99836751247d60
Admiral-Simo <admiralsimo16@gmail.com>
2024-07-20 00:47:02 +0100

    finish crypto package

--- crypto/keys_test.go
+++ crypto/keys_test.go
@@ -9,8 +9,45 @@ import (
 
 func TestGeneratePrivateKey(t *testing.T) {
 	privKey := GeneratePrivateKey()
-
 	assert.Equal(t, privKeyLen, len(privKey.Bytes()))
-	fmt.Println("privateKey :", privKey.Bytes())
-	fmt.Println("publicKey :", privKey.Public().key)
+
+	pubKey := privKey.Public()
+	assert.Equal(t, pubKeyLen, len(pubKey.Bytes()))
+}
+
+func TestNewPrivateKeyFromString(t *testing.T) {
+	var (
+		seed       = "6ba9a3d92b5a007f8790e51fd4de7544d7bc84149d7dd2e12dc4943f232937e0"
+		privKey    = NewPrivateKeyFromString(seed)
+		addressStr = "772d93d0c7cdd10f120eb19d70324436c2df20fa"
+	)
+	assert.Equal(t, privKeyLen, len(privKey.Bytes()))
+	address := privKey.Public().Address()
+	assert.Equal(t, addressStr, address.String())
+}
+
+func TestPrivateKeySign(t *testing.T) {
+	privKey := GeneratePrivateKey()
+	pubKey := privKey.Public()
+	msg := []byte("foo bar baz")
+
+	sig := privKey.Sign(msg)
+	assert.True(t, sig.Verify(pubKey, msg))
+
+	// test with invalid message
+	assert.False(t, sig.Verify(pubKey, []byte("foo")))
+
+	// test with invalid pub key
+	invalidPrivKey := GeneratePrivateKey()
+	invalidPubKey := invalidPrivKey.Public()
+	assert.False(t, sig.Verify(invalidPubKey, msg))
+}
+
+func TestPublicKeyToAddress(t *testing.T) {
+	privKey := GeneratePrivateKey()
+	pubKey := privKey.Public()
+	address := pubKey.Address()
+
+	assert.Equal(t, addressLen, len(address.Bytes()))
+	fmt.Println(address)
 }
